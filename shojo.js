0422ef00ccef7133174ee63b533a23c09e330136
NelsonMoreno1 <90421291+NelsonMoreno1@users.noreply.github.com>
2021-12-07 15:27:19 -0500

    first commit

---
+++ shojo.js
@@ -0,0 +1,4 @@
+let resultado = window.confirm('Bienvenidos a la seccion shojo, en esta pagina podras encontrar informacion de algunos animes del genero shojo no obstante tambien podras encontrar una pequeña sinopsis de estos mismos muchas gracias por leer espero que la disfrutes');
+if (resultado === true){
+    
+}
